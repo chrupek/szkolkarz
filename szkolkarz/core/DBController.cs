7c8bca88718bbfb08286d710ea07c58a311df620
chrupek <szczepan.dempniak@gmail.com>
2014-07-26 13:00:37 +0200

    refactor and developing functionality of reading from DB

--- szkolkarz/core/DBController.cs
+++ szkolkarz/core/DBController.cs
@@ -3,45 +3,33 @@ using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Text;
 using System.Data.Objects;
+using System.Linq;
 
 namespace szkolkarz.core
 {
-    class DBController : System.IDisposable
-    {
-        DBConnection dbConnection = new DBConnection();
-        utils.predefinedQueries queries = new utils.predefinedQueries();
-
-        public void createConnection()
+    class DBController : System.IDisposable{
+    
+        
+        public void Dispose()
         {
-            if (dbConnection.Equals(null))
-                dbConnection = new DBConnection();
-            dbConnection.openConnection();
+ 	        throw new NotImplementedException();
         }
+
+
 
         public List<ADM_SOWN_LOG> getSownHistory(string rowID)
         {
-            szkolkarzEntities dc = null;
+            int id = Convert.ToInt32(rowID);
 
-            try
-            {
-                dc = new szkolkarzEntities();
-                List<ADM_SOWN_LOG> sownLog = from c in dc.ADM_SOWN_LOG
-                                             select new
-                                             {
-                                                 rowID = c.ID,
-                                                 tapeID = c.TAPE_ID,
-                                                 year = c.YEAR,
-                                                 plantID = c.ADM_PLANT_ID
-                                             };
-                return sownLog;
-            }
-            finally
-            {
-                if (dc != null)
-                {
-                    dc = null;
-                }
-            }
+           using( var dbEntities = new szkolkarzEntities())
+           {
+               var query = from asw in
+                               dbEntities.ADM_SOWN_LOG
+                               where asw.ID == id      
+                           orderby asw.ADM_PLANT_ID
+                           select asw;
+              return query.ToList<ADM_SOWN_LOG>();
+           }
         }
     }
 }
--- szkolkarz/szkolkarzModel.Designer.cs
+++ szkolkarz/szkolkarzModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// Default code generation is disabled for model 'C:\Users\chrupek\workspace\szkolkarz\szkolkarz\szkolkarzModel.edmx'. 
+﻿// Default code generation is disabled for model 'F:\workspace\Szkolkarz\szkolkarz\SzkolkarzModel.edmx'. 
 // To enable default code generation, change the value of the 'Code Generation Strategy' designer
 // property to an alternate value. This property is available in the Properties Window when the model is
 // open in the designer.
