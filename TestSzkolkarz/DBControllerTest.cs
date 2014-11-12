667ca4821dbe9e8213f454442911a2977797f553
chrupek <szczepan.dempniak@gmail.com>
2014-11-12 16:51:37 +0100

    test class

---
+++ TestSzkolkarz/DBControllerTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using szkolkarz.core;
+using szkolkarz;
+
+namespace TestSzkolkarz
+{
+    [TestClass]
+    public class DBControllerTest
+    {
+        [TestMethod]
+        public void getSownHistoryTest()
+        {
+            List<int> ids = new List<int>{ 1, 2, 10, 15 };
+            DBController dataProvider = new DBController();
+            List<ADM_SOWN_LOG> result = dataProvider.getSownHistory(ids);
+        }
+    }
+}
